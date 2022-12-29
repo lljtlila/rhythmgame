e9b1da35385ab466eefb5ba7d70887deaed8bfe1
lljtlila <77391753+lljtlila@users.noreply.github.com>
2022-12-29 08:04:52 +0800

    df

--- 谱子.py
+++ 谱子.py
@@ -1,22 +1,31 @@
-import pygame,time,sys
+import pygame,time,sys,json
 a=[]
-b=0
+b=[]
+c=0
+e=0
+pygame.init()
+pygame.key.set_repeat(100, 150)
 clock=pygame.time.Clock()
-screen=pygame.display.set_mode((1280,720),0,32)
+screen=pygame.display.set_mode((100,100),0,32)
+pygame.mixer.init()
+pygame.mixer.music.load(".\\assets\\start\\start_game\\game\\arknight.wav")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(0)
 while True:
-    time.sleep(0.1)   
-    b=b+0.1
-    key=pygame.key.get_pressed() 
+    time.sleep(0.1)
+    c+0.1
+    c = round(c)
+    key=pygame.key.get_pressed()
     if key[pygame.K_f]:
-        a.append(b)
-    print(a)
-    for event in pygame.event.get():
-        
+        a.append(c)
+        print(c)
+    if key[pygame.K_j]:
+        b.append(c)
+        print(b)
+    for event in pygame.event.get():#傻逼罗智麒，我选择摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂摆烂
         if event.type==pygame.QUIT:
-            
             run=False
             pygame.quit()
             quit()
-      
     pygame.display.update()
     clock.tick(60)
