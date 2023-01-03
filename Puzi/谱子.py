933cc080d78717083ed576e3cf06ae4a4bece7c7
lljtlila <77391753+lljtlila@users.noreply.github.com>
2023-01-03 14:29:06 +0800

    dfg

--- Puzi/谱子.py
+++ Puzi/谱子.py
@@ -1,12 +1,15 @@
-import pygame,time,sys
+import pygame,time,sys,json
 from pygame.locals import*
 pygame.init()
 a=[]
 b=[]
 c=0
 clock=pygame.time.Clock()
-screen=pygame.display.set_mode((1280,720),0,32)
-
+screen=pygame.display.set_mode((300,200),0,32)
+pygame.mixer.init()
+pygame.mixer.music.load(".\\assets\\start\\start_game\\game\\arknight.wav")
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(0)
 while True:
     
     time.sleep(0.1)   
@@ -16,16 +19,19 @@ while True:
     if event.type==QUIT:
         pygame.quit()
         sys.exit()
-            
-
     if event.type == KEYDOWN and event.key==K_j:
         a.append(d)
+        print(a)
     if event.type==KEYDOWN and event.key==K_f:
         b.append(d)
+        print(b)
     if event.type ==KEYDOWN and event.key == K_RETURN:
         print("一线谱子为",a)
         print("二线谱子为",b)
-      
+        with open(".\\Puzi\\arknighta.json","w") as a2:
+            json.dump(a,a2)
+        with open(".\\Puzi\\arknightb.json","w") as b2:
+            json.dump(b,b2)
     pygame.display.update()
     clock.tick(60)
 
